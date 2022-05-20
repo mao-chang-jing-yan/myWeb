b9553332e99293ef7675b03c10a5fd80b232701b
mao <2425320239@qq.com>
2022-05-20 16:10:27 +0800

    revert

--- src/environments/apis.ts
+++ src/environments/apis.ts
@@ -1,6 +1,6 @@
-export const base = "http://127.0.0.1:8002/api/v1"
+// export const base = "http://127.0.0.1:8002/api/v1"
 // export const base = "http://192.168.31.61:8002/api/v1"
-// export const base = "http://47.100.82.173:8002/api/v1"
+export const base = "http://47.100.82.173:8002/api/v1"
 export const web = "/web"
 export const webBase = base + web
 export const wechat = "/wechat"
